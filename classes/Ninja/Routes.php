70296346625c3c669c35b767079bcfba844a9088
Mark XIe <84262040+markx35@users.noreply.github.com>
2022-10-13 15:33:49 -0700

    Delete classes directory

--- classes/Ninja/Routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Ninja;
-//05-12-22 yxie create two abstract methods for interface routes
-interface Routes {
-	public function getRoutes(): array;
-	public function getAuthentication(): \Ninja\Authentication;
-}
